40d0e5e59ce5996b503a97c301297ff2f95c1501
juelingyue <isahung001@gmail.com>
2024-08-23 20:34:24 +0800

    upload

--- app.py
+++ app.py
@@ -8,9 +8,9 @@ def home():
 
 @app.route('/news/<string:db_name>/<int:news_id>')
 def news_article(db_name, news_id):
-    # 在此函数中渲染静态页面，忽略数据库查询
-    title = f"News Title {news_id}"  # 只是演示，实际可以根据需要设定标题
-    content = "This is a static news article content."  # 静态内容
+    # Render static pages in this function, ignoring database queries
+    title = f"News Title {news_id}"  # example
+    content = "This is a static news article content."  # sstatic content
     return render_template('news.html', title=title, content=content)
 
 if __name__ == '__main__':

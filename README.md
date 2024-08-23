8dfae391f3f4cd8688690c5721286c372abbeef6
juelingyue <isahung001@gmail.com>
2024-08-24 02:14:57 +0800

    REDMD.md repair
    django model delete

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# linebot_news
+tst tensorboard --logdir=runs
+
+wget https://nlp.stanford.edu/data/glove.840B.300d.zip
+Credits
+Dataset by MIcrosoft News Dataset (MIND), see https://msnews.github.io/.
+Pretrained embedding by Stanford GloVe, see https://nlp.stanford.edu/projects/glove/.
--- voicelinebot/model.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
- 
- 
-class Movie(models.Model):
-    user_id = models.CharField(max_length=100000)  # 用戶id
-    item_id = models.CharField(max_length=100000)  # 電影id
-    rating = models.CharField(max_length=100000)  # 評分
-    timestamp = models.CharField(max_length=100000) # 時間戳

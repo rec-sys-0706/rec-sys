3585ad4f919d117a04b814b81759129af6a657df
unknown <lly.ke72@gmail.com>
2024-09-11 19:08:21 +0800

    修改部分檔案

--- website/main/routes.py
+++ website/main/routes.py
@@ -16,15 +16,7 @@ main_bp = Blueprint('main',
 
 @main_bp.route('/')
 def home():
-    news_date = test_news.sort_values('date').drop_duplicates(subset=['date'])
-    news = test_news.sort_values('title')
-    return render_template('index.html', news_date = news_date, news_article = news)
-
-@main_bp.route('/recommend')
-def recommend():
-    news_date = test_news.sort_values('date').drop_duplicates(subset=['date'])
-    news = test_news.sort_values('title')
-    return render_template('recommend.html', news_date = news_date, news_article = news)
+    return render_template('index.html')
 
 @main_bp.route('/recommend')
 def recommend():

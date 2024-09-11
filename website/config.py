f1e6c36b55a32c6af1dbdc4cd3495045e85b654c
unknown <lly.ke72@gmail.com>
2024-09-11 19:08:21 +0800

    修改部分檔案

--- website/config.py
+++ website/config.py
@@ -6,3 +6,8 @@ news = pd.read_csv('./website/admin/data/news.tsv',
                     index_col='news_id')
 
 result = pd.read_csv('./website/admin/data/result.csv', index_col='user_id')
+
+test_news = pd.read_csv('./website/admin/data/test_news.tsv',
+                    sep='\t',
+                    names=['news_id', 'category', 'subcategory', 'title', 'date'],
+                    index_col='news_id')

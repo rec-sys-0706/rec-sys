8fa8d38f4e67f68235b723af37d9d8dcfa89d6e0
unknown <lly.ke72@gmail.com>
2024-09-14 14:34:32 +0800

    修改讀取資料路徑

--- website/config.py
+++ website/config.py
@@ -1,4 +1,26 @@
+import requests
 import pandas as pd
+import hmac
+import hashlib
+from datetime import datetime
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def get_signature(payload=''):
+    # Get SQL_SECRET
+    #secret_key = '123'
+    secret_key = os.environ.get('SQL_SECRET')
+    # Compute the HMAC-SHA256 signature
+    signature = hmac.new(secret_key.encode('utf-8'), payload.encode('utf-8'), hashlib.sha256).hexdigest()
+    return signature    
+# payload = '{"example": "data"}' # 如果是 GET 則不用payload
+# Prepare the headers, including the x-hub-signature-256
+headers = {
+    'Content-Type': 'application/json',
+    'x-fju-signature-256': f'sha256={get_signature()}'
+}
 
 news = pd.read_csv('./website/admin/data/news.tsv',
                     sep='\t',
@@ -7,7 +29,9 @@ news = pd.read_csv('./website/admin/data/news.tsv',
 
 result = pd.read_csv('./website/admin/data/result.csv', index_col='user_id')
 
-test_news = pd.read_csv('./website/admin/data/test_news.tsv',
-                    sep='\t',
-                    names=['news_id', 'category', 'subcategory', 'title', 'date'],
-                    index_col='news_id')
+def format_date(date_str):
+    return datetime.strptime(date_str, "%a, %d %b %Y %H:%M:%S %Z").strftime("%b %d, %Y")
+
+response = requests.get('http://140.136.149.181:5000/api/news/', headers = headers)
+test_news = pd.DataFrame(response.json())
+test_news['date'] = test_news['date'].apply(format_date)
--- website/main/routes.py
+++ website/main/routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template
 from config import test_news
+import datetime
 
 main_bp = Blueprint('main', 
                     __name__, 
@@ -9,27 +10,17 @@ main_bp = Blueprint('main',
 
 @main_bp.route('/')
 def home():
-    news_date = test_news.sort_values('date').drop_duplicates(subset=['date'])
-    news = test_news.sort_values('title')
-    return render_template('index.html', news_date = news_date, news_article = news)
+    return render_template('index.html')
 
 @main_bp.route('/recommend')
 def recommend():
     news_date = test_news.sort_values('date').drop_duplicates(subset=['date'])
-    news = test_news.sort_values('title')
-    return render_template('recommend.html', news_date = news_date, news_article = news)
-
-@main_bp.route('/recommend')
-def recommend():
-    news_date = test_news.sort_values('date').drop_duplicates(subset=['date'])
-    news = test_news.sort_values('title')
-    return render_template('recommend.html', news_date = news_date, news_article = news)
-
-@main_bp.route('/recommend')
-def recommend():
-    news_date = test_news.sort_values('date').drop_duplicates(subset=['date'])
-    news = test_news.sort_values('title')
-    return render_template('recommend.html', news_date = news_date, news_article = news)
+    all_news = test_news.sort_values('title')
+    today = datetime.date.today()
+    #today_time = today.strftime('%b %d, %Y') 
+    today_time = today.strftime('%b 01, %Y') 
+    print(today_time)
+    return render_template('recommend.html', news_date = news_date, all_news = all_news, today_time = today_time)
 
 @main_bp.route('/news/<string:db_name>/<int:news_id>')
 def news_article(db_name, news_id):

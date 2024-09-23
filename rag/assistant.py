9126b605e90f1e4e134fd7840220e4cb71b25775
Elisa <ting20001024@yahoo.com.tw>
2024-09-23 19:38:00 +0800

    create assistant and tools

--- rag/assistant.py
+++ rag/assistant.py
@@ -34,6 +34,46 @@ if __name__ == '__main__':
     # create_assistant('Summarizer', """
     #     You are expert in analyzing and answering questions about any story, including fiction and non-fiction. You provide thoughtful, accurate answers based on the given content. You should focus on understanding key elements of the story, such as plot, characters, themes, settings, and the underlying message. You should give clear, concise answers to direct questions while providing detailed explanations when necessary. You should avoid adding details not present in the original story and refrain from interpreting beyond what is supported by the text. When clarification is needed, you should ask the user for more information or specific details to ensure accurate responses.
     #     """)
+    create_assistant("Intent_Recognizer",
+                     """You are message handler,and your task is to analyze the user's input and recognize their intent.
+                        Perform intent recognition based on user requests.  
+                        Ensure your reasoning is accurate before finalizing your output. Keep the tone direct and focused. 
+                        """)
+
+    tools = [
+    {
+        "type": "function",
+        "function": {
+            "name": 'search_keyword',
+            "description": "Perform a search based on user input.",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "content": {
+                        "type": "string",
+                        "description": "The search content provided by the user."
+                    }
+                },
+                "required": ["content"],
+                "additionalProperties": False
+            },
+            "strict": True
+        }
+    },
+    {
+        "type": "function",
+        "function": {
+            "name": 'no_this_function',
+            "description": "Responds with '不好意思，我沒有這個功能' if the request does not match any known function.",
+            "parameters": {
+                "type": "object",
+                "properties": {},
+                "additionalProperties": False
+            },
+            "strict": True
+        }
+    }
+]
 
 
     print(assistant)

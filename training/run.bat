27dcbc8f795fadf06741f12ecb9fda4476e22c7f
Sha-rky <sentientuwuartificial@gmail.com>
2024-10-01 00:11:50 +0800

    final_08

--- training/run.bat
+++ training/run.bat
@@ -3,5 +3,5 @@ python .\main.py --model-name NRMS-BERT ^
 --epochs 10 ^
 --learning-rate 3e-5 ^
 --pretrained-model-name "distilbert-base-uncased" ^
---train-batch-size 32 ^
---eval-batch-size 64
+--train-batch-size 16 ^
+--eval-batch-size 16

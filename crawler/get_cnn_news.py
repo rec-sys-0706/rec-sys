26c7f1dcd230e91286b7dfcc845f6ca4c3235a29
Elisa <ting20001024@yahoo.com.tw>
2024-10-03 20:37:50 +0800

    在cnn爬蟲檔加上儲存成csv的code

--- crawler/get_cnn_news.py
+++ crawler/get_cnn_news.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 import time
+import csv
 
 def setup_webdriver():
     service = Service()
@@ -20,17 +21,17 @@ def perform_search(driver, url, keyword):
     driver.find_element(By.XPATH, '//*[@id="search"]/div[1]/div[2]/div/div/ul/li[2]/label').click()
     time.sleep(10)
 
-def extract_articles(driver, page_count=1):
+def extract_articles(driver, csv_writer, page_count=1):
     for _ in range(page_count):
         content_div = driver.find_element(By.CLASS_NAME, 'container__field-links')
         divs_in_content = content_div.find_elements(By.XPATH, './div')
         div_count = len(divs_in_content)
         
         for x in range(1, div_count + 1):
-            extract_and_save_article(driver, x)
+            extract_and_save_article(driver, x, csv_writer)
         navigate_to_next_page(driver)
 
-def extract_and_save_article(driver, index):
+def extract_and_save_article(driver, index, csv_writer):
     titles = driver.find_element(By.XPATH, f'//*[@id="search"]/div[2]/div/div[2]/div/div[2]/div/div/div[{index}]/a[2]/div/div[1]/span').text
     publication_dates = driver.find_element(By.XPATH, f'//*[@id="search"]/div[2]/div/div[2]/div/div[2]/div/div/div[{index}]/a[2]/div/div[2]').text
     abstracts = driver.find_element(By.XPATH, f'//*[@id="search"]/div[2]/div/div[2]/div/div[2]/div/div/div[{index}]/a[2]/div/div[3]').text
@@ -40,6 +41,8 @@ def extract_and_save_article(driver, index):
     driver.get(urls)
     categories, subcategories = extract_categories(driver)
     content = extract_content(driver)
+
+    csv_writer.writerow([titles, publication_dates, abstracts, categories, subcategories, urls, content])
 
     driver.back()
     time.sleep(1)
@@ -77,8 +80,13 @@ def main():
     url = 'https://edition.cnn.com/'
     keyword = 'Artificial Intelligence'
     
-    perform_search(driver, url, keyword)
-    extract_articles(driver, page_count=4)  # 控制頁數
+    with open('articles.csv', mode='w', newline='', encoding='utf-8') as file:
+        csv_writer = csv.writer(file)
+        csv_writer.writerow(['Title', 'Publication Date', 'Abstract', 'Category', 'Subcategory', 'URL', 'Content'])
+    
+        perform_search(driver, url, keyword)
+        extract_articles(driver, csv_writer, page_count=4)  # 控制頁數
+        
     driver.quit()
 
 if __name__ == '__main__':

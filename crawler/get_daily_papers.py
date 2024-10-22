e4576b996de69e8b8cc082d05318dcb7a60f570b
Elisa <ting20001024@yahoo.com.tw>
2024-10-22 19:03:42 +0800

    爬蟲優化

--- crawler/get_daily_papers.py
+++ crawler/get_daily_papers.py
@@ -18,12 +18,13 @@ from recommendation import generate_random_scores
 import os, requests
 
 def scrape_huggingface_papers(output_file='output5.csv'):
-    response = requests.get(f"{os.environ.get('ROOT')}/api/user")
-    data = response.json()
-    users = data["data"]
+    # response = requests.get(f"{os.environ.get('ROOT')}/api/user")
+    # data = response.json()
+    # users = data["data"]
     
     driver = webdriver.Chrome()
     driver.get('https://huggingface.co/papers')
+    
 
     items_data = []
     seen = set()
@@ -63,84 +64,90 @@ def scrape_huggingface_papers(output_file='output5.csv'):
                 try:
                     title_element = article_element.find_element(By.TAG_NAME, 'h3')
                     title = title_element.text
-                    #print(title)
+                    print(title)
                     link = title_element.find_element(By.TAG_NAME, 'a').get_attribute('href')
-                    #print(link)
+                    print(link)
                 except StaleElementReferenceException:
                     print("元素已失效，重新嘗試抓取。")
                     continue
 
                 # 進入 arXiv 網頁抓取摘要與日期
                 arxiv = 'https://arxiv.org/abs/' + re.sub('/papers/', '', urlparse(link).path)
-                driver.get(arxiv)
+                driver.execute_script(f"window.open('{arxiv}', '_blank');")
+                driver.switch_to.window(driver.window_handles[1])
 
-                abstract = WebDriverWait(driver, 5).until(
-                    EC.presence_of_element_located((By.CLASS_NAME, 'abstract'))
-                ).text
-                gattered_datetime_element = driver.find_element(By.ID, 'content-inner')
-                gattered_datetime_original = gattered_datetime_element.find_element(By.CLASS_NAME, 'dateline').text
+                try:
+                    abstract = WebDriverWait(driver, 5).until(
+                        EC.presence_of_element_located((By.CLASS_NAME, 'abstract'))
+                    ).text
+                    print(abstract)
+                    gattered_datetime_element = driver.find_element(By.ID, 'content-inner')
+                    gattered_datetime_original = gattered_datetime_element.find_element(By.CLASS_NAME, 'dateline').text
 
-                # 使用正則表達式提取日期
-                pattern = r'\d{1,2} \w{3} \d{4}'
-                match = re.search(pattern, gattered_datetime_original)
-                date_str = match.group(0) if match else None
-                date_obj = datetime.strptime(date_str, "%d %b %Y")
-                gattered_datetime = date_obj.strftime("%Y-%m-%d %H:%M:%S")
-                #print(gattered_datetime)
+                    # 使用正則表達式提取日期
+                    pattern = r'\d{1,2} \w{3} \d{4}'
+                    match = re.search(pattern, gattered_datetime_original)
+                    date_str = match.group(0) if match else None
+                    date_obj = datetime.strptime(date_str, "%d %b %Y")
+                    gattered_datetime = date_obj.strftime("%Y-%m-%d %H:%M:%S")
+                    print(gattered_datetime)
 
-                # 防止重複記錄
-                record = (title, abstract, link, gattered_datetime)
+                    # 防止重複記錄
+                    record = (title, abstract, link, gattered_datetime)
+                    
+                    if title and abstract and link and gattered_datetime:
+                        if record not in seen:
+                            seen.add(record)
+                            items_list = {
+                                'uuid': str(uuid.uuid4()),
+                                'title': title,
+                                'abstract': abstract,
+                                'link': link,
+                                'data_source': 'hf_paper',
+                                'gattered_datetime': gattered_datetime
+                            }
+                            items_data.append(items_list)    
+                            items = [items_list]
+                                                
+                            # api_url = f"{os.environ.get('ROOT')}/api/item/crawler"
+                            # if api_url:  # 檢查環境變數是否存在
+                            #     item_post = requests.post(api_url, json=items_list, timeout=20) 
+                                
+                            #     if item_post.status_code == 201:
+                            #         recommendations = generate_random_scores(items,users)
+                            #         time.sleep(3)
+                            #         api_recommendations = f"{os.environ.get('ROOT')}/api/recommend/model"
+                            #         for recommendation in recommendations:
+                            #             recommendations_post = requests.post(api_recommendations, json=recommendation, timeout=30) 
+                            #             if recommendations_post.status_code == 201:
+                            #                 print(f"API 發送成功: {recommendations_post.text}")
                 
-                if title and abstract and link and gattered_datetime:
-                    if record not in seen:
-                        seen.add(record)
-                        items_list = {
-                            'uuid': str(uuid.uuid4()),
-                            'title': title,
-                            'abstract': abstract,
-                            'link': link,
-                            'data_source': 'hf_paper',
-                            'gattered_datetime': gattered_datetime
-                        }
-                        items_data.append(items_list)    
-                        items = [items_list]
-                                            
-                        api_url = f"{os.environ.get('ROOT')}/api/item/crawler"
-                        if api_url:  # 檢查環境變數是否存在
-                            item_post = requests.post(api_url, json=items_list, timeout=20) 
+                            #     if item_post.status_code != 201:
+                            #         print(f"API 發送失敗: {item_post.text}")
+                                
                             
-                            if item_post.status_code == 201:
-                                recommendations = generate_random_scores(items,users)
-                                time.sleep(3)
-                                api_recommendations = f"{os.environ.get('ROOT')}/api/recommend/model"
-                                for recommendation in recommendations:
-                                    recommendations_post = requests.post(api_recommendations, json=recommendation, timeout=30) 
-                                    if recommendations_post.status_code == 201:
-                                        print(f"API 發送成功: {recommendations_post.text}")
-            
-                            if item_post.status_code != 201:
-                                print(f"API 發送失敗: {item_post.text}")
-                            
-                        
-                        with open(output_file, mode='a', newline='', encoding='utf-8') as file:
-                            writer = csv.DictWriter(file, fieldnames=fieldnames)
-                            writer.writerow(items_list)
-                        
-                else:
+                            with open(output_file, mode='a', newline='', encoding='utf-8') as file:
+                                writer = csv.DictWriter(file, fieldnames=fieldnames)
+                                writer.writerow(items_list)
+                    else:
                         print(f"缺少資料: title={title}, abstract={abstract}, link={link}, gattered_datetime={gattered_datetime}")
-
-                driver.back()
-                time.sleep(1)
-
+                
+                except (NoSuchElementException, TimeoutException) as e:
+                    print(f"跳過該元素，原因：{e}")
+                    
+                driver.close()
+                driver.switch_to.window(driver.window_handles[0])
+                time.sleep(1)    
+                
             except (NoSuchElementException, TimeoutException) as e:
                 print(f"跳過該元素，原因：{e}")
-                continue
-
+                continue    
+            
         # 處理換頁按鈕
         try:
             next_page_button = driver.find_element(By.LINK_TEXT, 'Previous')
             next_page_button.click()
-            time.sleep(3)
+            time.sleep(1)
         except NoSuchElementException:
             print("沒有更多頁面了，結束爬取。")
             break

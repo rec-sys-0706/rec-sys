afb854ca73ba89d1a2a8c6e48320bd94721a0a94
Elisa <ting20001024@yahoo.com.tw>
2024-10-22 10:18:19 +0800

    爬蟲+api傳值程式碼更新

--- crawler/get_bbc_news.py
+++ crawler/get_bbc_news.py
@@ -18,6 +18,10 @@ from selenium.webdriver.support import expected_conditions as EC
 from recommendation import generate_random_scores
 
 def scrape_bbc_articles(output_file='output6.csv'):
+    response = requests.get(f"{os.environ.get('ROOT')}/api/user")
+    data = response.json()
+    users = data["data"]
+    
     driver = webdriver.Chrome()
     driver.get('https://www.bbc.com/search?q=Artificial%20Intelligence&edgeauth=eyJhbGciOiAiSFMyNTYiLCAidHlwIjogIkpXVCJ9.eyJrZXkiOiAiZmFzdGx5LXVyaS10b2tlbi0xIiwiZXhwIjogMTcyOTQzMDIyMiwibmJmIjogMTcyOTQyOTg2MiwicmVxdWVzdHVyaSI6ICIlMkZzZWFyY2glM0ZxJTNEQXJ0aWZpY2lhbCUyNTIwSW50ZWxsaWdlbmNlIn0.mUpWVN3N9gvpw2DpJz8UhFLrxTbWLb17ZWLp81mNGi4')
 
@@ -41,20 +45,21 @@ def scrape_bbc_articles(output_file='output6.csv'):
 
         for div_element in div_elements:
             try:
-                title = div_element.find_element(By.CLASS_NAME, 'sc-1207bea1-3').text
-                print(title)
+                time.sleep(1)
+                title = div_element.find_element(By.CLASS_NAME, 'sc-8ea7699c-3').text
+                #print(title)
 
                 abstract = div_element.find_element(By.CLASS_NAME, 'sc-ae29827d-0').text
-                print(abstract)
+                #print(abstract)
                 
                 link = div_element.find_element(By.TAG_NAME, 'a').get_attribute('href')
-                print(link)
+                #print(link)
 
                 try:
-                    date_str = div_element.find_element(By.CLASS_NAME, 'sc-4e537b1-1').text
+                    date_str = div_element.find_element(By.CLASS_NAME, 'sc-6fba5bd4-1').text
                     date_obj = datetime.strptime(date_str, "%d %b %Y")
                     gattered_datetime = date_obj.strftime("%Y-%m-%d %H:%M:%S")
-                    print(gattered_datetime)
+                    #print(gattered_datetime)
                 except ValueError:
                     print(f"日期格式錯誤，跳過：{date_str}")
                     continue  # 跳過該筆資料
@@ -64,7 +69,7 @@ def scrape_bbc_articles(output_file='output6.csv'):
                 if title and abstract and link and gattered_datetime:
                     if record not in seen:
                         seen.add(record)
-                        items = {
+                        items_list = {
                             'uuid': str(uuid.uuid4()),
                             'title': title,
                             'abstract': abstract,
@@ -72,20 +77,29 @@ def scrape_bbc_articles(output_file='output6.csv'):
                             'data_source': 'bbc_news',
                             'gattered_datetime': gattered_datetime
                         }
-                        items_data.append(items)
+                        items_data.append(items_list)
+                        items = [items_list]
                         
-                        api_url = f"{os.environ.get('ROOT')}:5000/api/item/crawler"
-                        if api_url:  
-                            item_post = requests.post(api_url, json=items, timeout=10) 
+                        api_url = f"{os.environ.get('ROOT')}/api/item/crawler"
+                        if api_url:  # 檢查環境變數是否存在
+                            item_post = requests.post(api_url, json=items_list, timeout=20) 
+                            
                             if item_post.status_code == 201:
-                                generate_random_scores(items,users)
+                                recommendations = generate_random_scores(items,users)
+                                time.sleep(3)
+                                api_recommendations = f"{os.environ.get('ROOT')}/api/recommend/model"
+                                for recommendation in recommendations:
+                                    recommendations_post = requests.post(api_recommendations, json=recommendation, timeout=30) 
+                                    if recommendations_post.status_code == 201:
+                                        print(f"API 發送成功: {recommendations_post.text}")
+            
                             if item_post.status_code != 201:
                                 print(f"API 發送失敗: {item_post.text}")
                             
                         
                         with open(output_file, mode='a', newline='', encoding='utf-8') as file:
                             writer = csv.DictWriter(file, fieldnames=fieldnames)
-                            writer.writerow(items)
+                            writer.writerow(items_list)
                         
                 else:
                         print(f"缺少資料: title={title}, abstract={abstract}, link={link}, gattered_datetime={gattered_datetime}")
@@ -105,3 +119,6 @@ def scrape_bbc_articles(output_file='output6.csv'):
             break
 
     driver.quit()
+    
+scrape_bbc_articles()
+    

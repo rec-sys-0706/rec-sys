c47c0b9417e319509276cc744314b84943e9e70a
fjuser <fju@edu.com>
2024-11-01 22:40:01 +0800

    modify api

--- server/services/browsing_history.py
+++ server/services/browsing_history.py
@@ -1,10 +1,10 @@
 import logging
-from datetime import date, datetime, timedelta
+from datetime import datetime, timedelta
 from apiflask import APIBlueprint, abort
 from flask import jsonify, request
 from flask_jwt_extended import jwt_required
-from sqlalchemy import DATE, func
 
+from config import DB
 from server.models.behavior import Behavior
 from server.models.recommendation_log import Recommendationlog
 from server.models.item import Item
@@ -36,14 +36,19 @@ def read_browsing_history(user_uuid):
 
     return jsonify({'history': result}), 200
 
+
 @user_history_bp.route('/recommend/<uuid:user_uuid>', methods = ['GET'])
 def get_recommend_items(user_uuid):
     try:
-        # search for user_uuid: recommend_score = 1
-        recommendation_logs = Recommendationlog.query.filter_by(
+
+        data_source = request.args.get('data_source', None)
+
+        recommendation_logs_query = Recommendationlog.query.filter_by(
             user_id=user_uuid,
             recommend_score=True
-        ).all()
+        )
+
+        recommendation_logs = recommendation_logs_query.all()
 
         if not recommendation_logs:
             return jsonify({'message': 'No recommendations found'}), 404
@@ -51,17 +56,19 @@ def get_recommend_items(user_uuid):
         # 根據 recommendation_log 中的 item_id 查詢對應的 item 資料，並返回 recommendation_log 的 uuid
         result = []
         for log in recommendation_logs:
-            item = Item.query.filter_by(uuid=log.item_id).first()
+            item_query = Item.query.filter_by(uuid=log.item_id)
+            if data_source:
+                item_query = item_query.filter_by(data_source=data_source)
+
+            item = item_query.order_by(Item.gattered_datetime.desc()).limit(10).all()
             if item:
-                result.append({
+                result.extend({
                     'recommendation_log_uuid': log.uuid,
-                    'item': item.serialize()
-                })
+                    'item': i.serialize()
+                } for i in item)
 
-        result.sort(key=lambda x: x['item']['gattered_datetime'], reverse=True)
-        result_top50 = result[:50]
 
-        return jsonify(result_top50), 200
+        return jsonify(result[:10]), 200
 
     except Exception as error:
         logging.error(f'[GET ERROR] - {error}')
@@ -71,11 +78,14 @@ def get_recommend_items(user_uuid):
 @user_history_bp.route('/unrecommend/<uuid:user_uuid>', methods = ['GET'])
 def get_unrecommend_items(user_uuid):
     try:
-        # search for user_uuid: recommend_score = 0
-        recommendation_logs = Recommendationlog.query.filter_by(
-            user_id=user_uuid,
-            recommend_score=False
-        ).all()
+        data_source = request.args.get('data_source', None)
+
+        recommendation_logs_query = Recommendationlog.query.filter_by(
+            user_id = user_uuid,
+            recommend_score = False
+        )
+
+        recommendation_logs = recommendation_logs_query.all()
 
         if not recommendation_logs:
             return jsonify({'message': 'No recommendations found'}), 404
@@ -83,57 +93,83 @@ def get_unrecommend_items(user_uuid):
         # 根據 recommendation_log 中的 item_id 查詢對應的 item 資料，並返回 recommendation_log 的 uuid
         result = []
         for log in recommendation_logs:
-            item = Item.query.filter_by(uuid=log.item_id).first()
+            item_query = Item.query.filter_by(uuid=log.item_id)
+            if data_source:
+                item_query = item_query.filter_by(data_source=data_source)
+
+            item = item_query.order_by(Item.gattered_datetime.desc()).limit(10).all()
             if item:
-                result.append({
+                result.extend({
                     'recommendation_log_uuid': log.uuid,
-                    'item': item.serialize(),
-                })
+                    'item': i.serialize()
+                } for i in item)
 
-        result.sort(key=lambda x: x['item']['gattered_datetime'], reverse=True)
-
-        result_top50 = result[:50]
-
-        return jsonify(result_top50), 200
+        return jsonify(result[:10]), 200
 
     except Exception as error:
         logging.error(f'[GET ERROR] - {error}')
         abort(500)
 
 
-####只獲得當週推薦的新聞
-@user_history_bp.route('/week-news/<uuid:user_uuid>', methods=['GET'])
-def get_today_recommendations(user_uuid):
+####當週推薦的新聞
+@user_history_bp.route('/recommend_week/<uuid:user_uuid>', methods=['GET'])
+def get_week_recommendations(user_uuid):
     try:
-        # today
+        # this week
         today = datetime.today() + timedelta(days=1)
         start_of_week = today - timedelta(days=7)
 
-        recommendation_logs = Recommendationlog.query.filter_by(
-            user_id=user_uuid,
-            recommend_score = True
-        ).all()
+        recommendation_logs = DB.session.query(Recommendationlog, Item)\
+            .join(Item, Recommendationlog.item_id == Item.uuid)\
+            .filter(
+                Recommendationlog.user_id == user_uuid,
+                Recommendationlog.recommend_score == True,
+                Item.gattered_datetime >= start_of_week,
+                Item.gattered_datetime <= today
+            ).order_by(Item.gattered_datetime).limit(10).all()
 
         if not recommendation_logs:
-            return jsonify({'message': 'No recommendations found for today'}), 404
+            return jsonify({'message': 'No recommendations found for this week'}), 404
         
-        result = []
-        for log in recommendation_logs:
-            item = Item.query.filter_by(uuid=log.item_id).filter(
-                Item.gattered_datetime >= start_of_week,
-                Item.gattered_datetime < today
-            ).order_by(Item.gattered_datetime.desc()).first()  
-            if item:
-                result.append({
-                    'recommendation_log_uuid': log.uuid,
-                    'item': item.serialize(),
-                })
-
-        if not result:
-            return jsonify({'message': 'No news found for today'}), 404
+        result = [{
+            'recommendation_log_uuid': log.uuid,
+            'item': item.serialize()
+        } for log, item in recommendation_logs]
         
         return jsonify(result), 200
 
+    except Exception as error:
+        logging.error(f'[GET ERROR] - {error}')
+        abort(500)
+
+
+####當週不推的新聞
+@user_history_bp.route('/unrecommend_week/<uuid:user_uuid>', methods=['GET'])
+def get_week_unrecommendations(user_uuid):
+    try:
+        # this week
+        today = datetime.today() + timedelta(days=1)
+        start_of_week = today - timedelta(days=7)
+
+        recommendation_logs = DB.session.query(Recommendationlog, Item)\
+            .join(Item, Recommendationlog.item_id == Item.uuid)\
+            .filter(
+                Recommendationlog.user_id == user_uuid,
+                Recommendationlog.recommend_score == False,
+                Item.gattered_datetime >= start_of_week,
+                Item.gattered_datetime <= today
+            ).order_by(Item.gattered_datetime).limit(10).all()
+
+        if not recommendation_logs:
+            return jsonify({'message': 'No recommendations found for this week'}), 404
+        
+        result = [{
+            'recommendation_log_uuid': log.uuid,
+            'item': item.serialize()
+        } for log, item in recommendation_logs]
+        
+        return jsonify(result), 200
+    
     except Exception as error:
         logging.error(f'[GET ERROR] - {error}')
         abort(500)

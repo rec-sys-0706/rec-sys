231355949b07cb8c7eadbd97e880686a4f065a4f
Sha-rky <114484390+Sha-rky@users.noreply.github.com>
2024-11-01 22:28:35 +0800

    Website04 (#54)
    1. 連接資料庫
    2. 登入註冊資料抓取
    3. 今天新聞頁面點擊紀錄
    4. 新增user修改資料功能
    
    ---------
    
    Co-authored-by: unknown <lly.ke72@gmail.com>

--- website/main/routes.py
+++ website/main/routes.py
@@ -1,12 +1,11 @@
-from flask import Blueprint, render_template, send_file, request
-from config import test_news, headers, ROOT
+from flask import Blueprint, render_template, request, session, redirect
+from config import register, item_data, login, BASE_URL, click_data, user_data, update_user_data, msg, get_recommend, get_unrecommend, get_user_cliked
 import matplotlib.pyplot as plt
 import io
 from PIL import Image
 from collections import Counter
-import datetime
+from datetime import date
 import re
-import requests
 import pandas as pd
 
 main_bp = Blueprint('main', 
@@ -20,23 +19,15 @@ main_bp = Blueprint('main',
 def index():
     status = 'T'
     if request.method == 'POST':
-        username = request.form['username']
+        account = request.form['account']
         password = request.form['password']
-        data = {
-            "account": username,
-            "password": password
-        }
-        responses = requests.get(f'{ROOT}:5000/api/user/verification', headers = headers, json = data)
-        message = responses.json()
-        if message['message'] == 'User not found':
+        msg = login(account, password)
+        if msg == None:
             status = 'F'
         else:
+            session['token'] = msg
             return render_template('./recommend/about.html')
     return render_template('./main/login.html', status = status)
-
-@main_bp.route('/about')
-def about():
-    return render_template('./main/about.html')
 
 def is_valid_email(email):
     #電子郵件格式
@@ -52,142 +43,88 @@ def signup():
     status = 'T'
     if request.method == 'POST':
         email = request.form['email']
-        username = request.form['username']
+        account = request.form['account']
         password = request.form['password']
 
         if is_valid_email(email):
             status = 'True'
-            data = {
-                "account": username,
-                "password": password,
-                "email": email
-            }
-            requests.post(f'{ROOT}:5000/api/user/', headers = headers, json = data)
+            text = register(email, account, password)
+            message = msg(text)
+            if(message == 'exists'):
+                status = 'F'
         else:
-            status = 'False'
-        
+            status = 'False' 
     return render_template('./main/signup.html', status = status)
-
 
 # recommend 資料夾
 @main_bp.route('/recommend')
 def recommend():
     return render_template('./recommend/about.html')
 
-news_dates = test_news.sort_values('date').drop_duplicates(subset=['date'])
-all_news = test_news.sort_values('title')
-
-@main_bp.route('/today_news')
+@main_bp.route('/today_news', methods = ['GET','POST'])
 def today_news():
-    today = datetime.date.today()
-    today_time = today.strftime('%b %d, %Y')
-    return render_template('./recommend/today_news.html', news_date = news_dates, 
-                           today_time = today_time, all_news = all_news)
+    if 'token' in session:
+        if request.method == 'POST':
+            data = request.get_json()
+            link = data.get('link')
+            click_data(session['token'], link)
+        recommend_new = get_recommend(session['token'])
+        unrecommend_news = get_unrecommend(session['token'])
+        return render_template('./recommend/today_news.html', recommend_new = recommend_new, unrecommend_news = unrecommend_news)
+    else:
+        return redirect(f'{BASE_URL}:8080/main')
 
 @main_bp.route('/all_dates')
 def all_dates():
-    return render_template('./recommend/all_dates.html', news_date = news_dates)
+    if 'token' in session:
+        all_news = item_data()
+        news_dates = all_news.sort_values('gattered_datetime').drop_duplicates(subset=['gattered_datetime'])
+        return render_template('./recommend/all_dates.html', news_date = news_dates)        
+    else:
+        return redirect(f'{BASE_URL}:8080/main')
 
 @main_bp.route('/all_news')
 def allnews():
-    date = request.args.get('date')
-    news_date = test_news.loc[test_news['date'] == date]
-    return render_template('./recommend/all_news.html', news_date = news_date, all_news = all_news)
+    if 'token' in session:
+        all_news = item_data()
+        date = request.args.get('gattered_datetime')
+        date_news = all_news.loc[all_news['gattered_datetime'] == date]
+        return render_template('./recommend/all_news.html', all_news = date_news)        
+    else:
+        return redirect(f'{BASE_URL}:8080/main')
 
 @main_bp.route('/profile')
 def profile():
-    return render_template('./recommend/profile.html', user_info = user_info)
+    if 'token' in session:
+        user = user_data(session['token'])
+        history = get_user_cliked(session['token'])
+        return render_template('./recommend/profile.html', user_data = user, history = history)
+    else:
+        return redirect(f'{BASE_URL}:8080/main')
 
 @main_bp.route('/revise', methods = ['GET','POST'])
 def revise():
+    if 'token' in session:
+        user = user_data(session['token'])
+        history = get_user_cliked(session['token'])
     status = 'T'
     if request.method == 'POST':
-        username = request.form['username']
+        account = request.form['account']
         password = request.form['password']
         email = request.form['email']
-        phone = request.form['phone']
+        line_id = request.form['line_id']
         
         if is_valid_email(email):
             status = 'True'
+            update_user_data(session['token'], account, password, email, line_id)
         else:
             status = 'False'
-    return render_template('./recommend/revise.html', status = status)
+    return render_template('./recommend/revise.html', status = status, user_data = user, history = history)
+
 
 @main_bp.route('/news/<string:db_name>/<int:news_id>')
 def news_article(db_name, news_id):
     # Render static pages in this function, ignoring database queries
     title = f"News Title {news_id}"  # example
     content = "This is a static news article content."  # static content
-    return render_template('news.html', title=title, content=content)
-
-# Sample news data
-articles = [
-    {
-        'category': 'Breaking News',
-        'title': 'Breaking News: Market Hits All-Time High',
-        'content': 'The stock market has reached an all-time high today, with major indices showing significant gains.',
-        'author': 'John Doe',
-        'date': '2024-08-09'
-    },
-    {
-        'category': 'Tech Innovations',
-        'title': 'Tech Innovations: AI Revolutionizing Industries',
-        'content': 'Artificial Intelligence is transforming the way businesses operate, from automation to customer service.',
-        'author': 'Jane Smith',
-        'date': '2024-08-08'
-    },
-    {
-        'category': 'Breaking News',
-        'title': 'Breaking News: Market Hits All-Time High',
-        'content': 'The stock market has reached an all-time high today, with major indices showing significant gains.',
-        'author': 'John Doe',
-        'date': '2024-08-09'
-    },
-    {
-        'category': 'Tech Innovations',
-        'title': 'Tech Innovations: AI Revolutionizing Industries',
-        'content': 'Artificial Intelligence is transforming the way businesses operate, from automation to customer service.',
-        'author': 'Jane Smith',
-        'date': '2024-08-08'
-    },
-    {
-        'category': 'Health Update',
-        'title': 'Health Update: New Breakthrough in Cancer Research',
-        'content': 'Scientists have announced a major breakthrough in cancer treatment, promising better outcomes for patients.',
-        'author': 'Alice Brown',
-        'date': '2024-08-07'
-    },
-]
-
-user_info = {
-        'Account': 'John',
-        'password': 'xxxxxxxxx',
-        'email': 'johndoe@example.com',
-        'phone': '123-456-7890'   
-}
-
-@main_bp.route('/donut_chart.png')
-def donut_chart():
-    categories = [article['category'] for article in articles]
-    category_counts = Counter(categories)
-
-    labels = category_counts.keys()
-    sizes = category_counts.values()
-
-    # Create a pie chart with a hole in the center (donut chart)
-    fig, ax = plt.subplots()
-    ax.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=90, wedgeprops={'width': 0.4})
-
-    # Draw a circle in the center to make it a donut chart
-    center_circle = plt.Circle((0, 0), 0.70, fc='white')
-    fig.gca().add_artist(center_circle)
-
-    # Ensure the chart is a circle
-    ax.axis('equal')
-
-    # Save the chart to a BytesIO object
-    img = io.BytesIO()
-    plt.savefig(img, format='png')
-    img.seek(0)
-
-    return send_file(img, mimetype='image/png')
+    return render_template('./news.html', title=title, content=content)

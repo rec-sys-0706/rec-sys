ccf2e0e1a71c1c0be2028a85efa61835d29535a7
LeeChenNing <nicolte-rabbit@hotmail.com>
2024-11-04 17:02:41 +0800

    update_server

--- app.py
+++ app.py
@@ -39,4 +39,4 @@ def create_app():
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO, format='[%(levelname)s] - %(message)s')
     app = create_app()
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host='0.0.0.0', port=80, debug=False)
